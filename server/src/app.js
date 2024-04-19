9b5d69c432976c11971d49d023e3a484a33c7ef5
aryakarthi <aryaperumal.m@gmail.com>
2024-04-19 22:19:45 +0530

    added: cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -14,6 +15,19 @@ const PORT = 5000;
 
 const app = express();
 
+const whitelist = ["http://localhost:5173", "http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
